f5c43e3211535924df7f5fc76958e1cbb451d9a1
electrosalaf <lawalibrahimayodeji@gmail.com>
2022-10-05 08:41:21 +0100

    added enums for notification type

---
+++ src/main/java/io/electrosalaf/reward_your_teacher/domain/entities/enums/NotificationType.java
@@ -0,0 +1,7 @@
+package io.electrosalaf.reward_your_teacher.domain.entities.enums;
+
+public enum NotificationType {
+    CREDIT_NOTIFICATION,
+    DEBIT_NOTIFICATION,
+    APPRECIATION_NOTIFICATION
+}
