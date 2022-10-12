ff12d22cb5d6b6809f747ecc167d0412daf11b43
electrosalaf <lawalibrahimayodeji@gmail.com>
2022-10-12 22:42:12 +0100

    implemented method to get logged in user details

---
+++ src/main/java/io/electrosalaf/reward_your_teacher/infrastructure/configuration/security/UserDetails.java
@@ -0,0 +1,15 @@
+package io.electrosalaf.reward_your_teacher.infrastructure.configuration.security;
+
+import io.electrosalaf.reward_your_teacher.infrastructure.error_handler.CustomNotFoundException;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class UserDetails {
+    public static String getLoggedInUserDetails(){
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if (!(principal instanceof org.springframework.security.core.userdetails.UserDetails)) {
+            throw new CustomNotFoundException("user not found");
+        }
+        return ((org.springframework.security.core.userdetails.UserDetails)principal).getUsername();
+    }
+}
