59dd41a2e278365687ff5fe5f6be7543fe146283
electrosalaf <lawalibrahimayodeji@gmail.com>
2022-10-12 22:14:31 +0100

    added abstract entity to serve as base entity

---
+++ src/main/java/io/electrosalaf/reward_your_teacher/domain/entities/AbstractEntity.java
@@ -0,0 +1,35 @@
+package io.electrosalaf.reward_your_teacher.domain.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@MappedSuperclass //TO ENABLE THE ABSTRACT CLASS FIELDS SHOW ON THE DB
+public abstract class AbstractEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    public AbstractEntity(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
