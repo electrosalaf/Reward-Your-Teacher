5aee63297f41a0411d53a849dfb4fe4c76811b69
electrosalaf <lawalibrahimayodeji@gmail.com>
2022-10-12 22:12:18 +0100

    added chat entity

---
+++ src/main/java/io/electrosalaf/reward_your_teacher/domain/entities/message/ChatEntity.java
@@ -0,0 +1,44 @@
+package io.electrosalaf.reward_your_teacher.domain.entities.message;
+
+import io.electrosalaf.reward_your_teacher.domain.entities.AbstractEntity;
+import io.electrosalaf.reward_your_teacher.domain.entities.StudentEntity;
+import io.electrosalaf.reward_your_teacher.domain.entities.TeacherEntity;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "chat")
+public class ChatEntity extends AbstractEntity {
+
+    @Column(
+            name = "message",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    private String message;
+
+    @ManyToOne
+    @JoinColumn(name = "teacher_chat_id")
+    private TeacherEntity teacher_id;
+
+    @ManyToOne
+    @JoinColumn(name = "student_chat_id")
+    private StudentEntity student_id;
+
+    @CreationTimestamp
+    @Column(name = "send_date", nullable = false, updatable = false)
+    private LocalDateTime sendDate;
+
+    @CreationTimestamp
+    @Column(name = "received_date", nullable = false, updatable = false)
+    private LocalDateTime receivedDate;
+
+}
