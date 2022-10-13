f2481164b475e69ae519dbffec7737c9d1cfb15e
electrosalaf <lawalibrahimayodeji@gmail.com>
2022-10-13 01:38:09 +0100

    implemented exception to handle invalid JWT

---
+++ src/main/java/io/electrosalaf/reward_your_teacher/infrastructure/error_handler/JwtInvalidException.java
@@ -0,0 +1,7 @@
+package io.electrosalaf.reward_your_teacher.infrastructure.error_handler;
+
+public class JwtInvalidException extends RuntimeException {
+    public JwtInvalidException(String message) {
+        super(message);
+    }
+}
